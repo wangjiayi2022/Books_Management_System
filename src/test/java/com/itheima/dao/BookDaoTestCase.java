c93b1bcf6901a2fffbae5d2e015267c2c9878218
lucy <wangjy1024@foxmail.com>
2022-02-22 21:51:13 +0800

    end version

---
+++ src/test/java/com/itheima/dao/BookDaoTestCase.java
@@ -0,0 +1,79 @@
+package com.itheima.dao;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+//dao层测试
+@SpringBootTest
+public class BookDaoTestCase {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testGetById() {
+        System.out.println(bookDao.selectById(2));
+    }
+
+    @Test
+    void testSave() {
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookDao.insert(book);
+    }
+
+    @Test
+    void testUpdate() {
+        Book book = new Book();
+        book.setId(11);
+        book.setType("测试数据123456");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123xxx");
+        bookDao.updateById(book);
+    }
+
+    @Test
+    void testDelete() {
+        bookDao.deleteById(11);
+    }
+
+    @Test
+    void testGetAll() {
+        bookDao.selectList(null);
+    }
+
+    @Test
+    void testGetPage() {
+        IPage page = new Page(2, 5);
+        bookDao.selectPage(page, null);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        System.out.println(page.getRecords());
+    }
+
+    @Test
+    void testGetBy() {
+        QueryWrapper<Book> qw = new QueryWrapper<>();
+        qw.like("name", "Spring");
+        bookDao.selectList(qw);
+    }
+
+    @Test
+    void testGetBy2() {
+        String name = "Spring";
+        LambdaQueryWrapper<Book> lqw = new LambdaQueryWrapper<>();
+//        if(name != null) lqw.like(Book::getName,name);
+        lqw.like(name != null, Book::getName, name);
+        bookDao.selectList(lqw);
+    }
+
+}
